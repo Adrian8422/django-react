50982192c9df0410b6c65b820eb93ba8cf103242
Adrian Villegas <manbassman1996@gmail.com>
2023-08-28 14:15:37 -0300

    testing

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,6 +1,6 @@
 const BASE_URL =
-  process.env.NODE_ENV == "production"
-    ? "https://django-react-production-1b55.up.railway.app"
+  process.env.NODE_ENV === "production"
+    ? process.env.VITE_BACKEND_URL
     : "http://localhost:8000";
 
 import axios from "axios";
@@ -10,16 +10,44 @@ const tasksApi = axios.create({
 });
 
 function getAllTasks() {
-  return tasksApi.get(BASE_URL + "tasks/");
+  console.log("url", BASE_URL);
+
+  return tasksApi.get("tasks/");
 }
 
 function createTask(task) {
-  tasksApi.post(BASE_URL + "tasks/", task);
+  tasksApi.post("tasks/", task);
 }
-const getTask = (id) => tasksApi.get(BASE_URL + `tasks/${id}`);
 
-const deleteTask = (id) => tasksApi.delete(BASE_URL + "tasks/" + id);
-const updateTask = (id, task) =>
-  tasksApi.patch(BASE_URL + `tasks/${id}/`, task);
+const getTask = (id) => tasksApi.get(`tasks/${id}`);
+
+const deleteTask = (id) => tasksApi.delete("tasks/" + id);
+const updateTask = (id, task) => tasksApi.patch(`tasks/${id}/`, task);
 
 export { getAllTasks, createTask, deleteTask, updateTask, getTask };
+// const BASE_URL =
+//   process.env.NODE_ENV == "production"
+//     ? import.meta.env.VITE_BACKEND_URL
+//     : "http://localhost:8000";
+
+// import axios from "axios";
+
+// const tasksApi = axios.create({
+//   baseURL: BASE_URL + "/tasks/api/v1/",
+// });
+
+// function getAllTasks() {
+//   console.log("url", BASE_URL);
+//   return tasksApi.get(BASE_URL + "tasks/");
+// }
+
+// function createTask(task) {
+//   tasksApi.post(BASE_URL + "tasks/", task);
+// }
+// const getTask = (id) => tasksApi.get(BASE_URL + `tasks/${id}`);
+
+// const deleteTask = (id) => tasksApi.delete(BASE_URL + "tasks/" + id);
+// const updateTask = (id, task) =>
+//   tasksApi.patch(BASE_URL + `tasks/${id}/`, task);
+
+// export { getAllTasks, createTask, deleteTask, updateTask, getTask };
