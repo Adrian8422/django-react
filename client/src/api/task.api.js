86354e841bf7023dc7027fb93736bd771a10184d
Adrian Villegas <manbassman1996@gmail.com>
2023-08-28 15:48:24 -0300

    add csrf

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -28,29 +28,3 @@ const deleteTask = (id) => tasksApi.delete("tasks/" + id);
 const updateTask = (id, task) => tasksApi.patch(`tasks/${id}/`, task);
 
 export { getAllTasks, createTask, deleteTask, updateTask, getTask };
-// const BASE_URL =
-//   process.env.NODE_ENV == "production"
-//     ? import.meta.env.VITE_BACKEND_URL
-//     : "http://localhost:8000";
-
-// import axios from "axios";
-
-// const tasksApi = axios.create({
-//   baseURL: BASE_URL + "/tasks/api/v1/",
-// });
-
-// function getAllTasks() {
-//   console.log("url", BASE_URL);
-//   return tasksApi.get(BASE_URL + "tasks/");
-// }
-
-// function createTask(task) {
-//   tasksApi.post(BASE_URL + "tasks/", task);
-// }
-// const getTask = (id) => tasksApi.get(BASE_URL + `tasks/${id}`);
-
-// const deleteTask = (id) => tasksApi.delete(BASE_URL + "tasks/" + id);
-// const updateTask = (id, task) =>
-//   tasksApi.patch(BASE_URL + `tasks/${id}/`, task);
-
-// export { getAllTasks, createTask, deleteTask, updateTask, getTask };
